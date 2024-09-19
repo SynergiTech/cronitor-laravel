b6c0b2384540cfb563534f7e5dc6b17bda444ca0
Will Power <1619102+willpower232@users.noreply.github.com>
2024-09-19 11:52:04 +0100

    Update Cronitor.php

--- src/Facades/Cronitor.php
+++ src/Facades/Cronitor.php
@@ -5,6 +5,10 @@ namespace SynergiTech\Cronitor\Laravel\Facades;
 use Illuminate\Support\Facades\Facade;
 use SynergiTech\Cronitor\Laravel\Cronitor as AccessorClass;
 
+/**
+ * @method static bool telemetryEnabled()
+ * @method static mixed monitorJob(string $monitorKey, callable $job)
+ */
 class Cronitor extends Facade
 {
     protected static function getFacadeAccessor()
